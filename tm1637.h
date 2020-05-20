6ccf89c1c2f91e1246228312782a9d903e81094f
XuJiacheng <42736100+xjcdxj@users.noreply.github.com>
2020-05-21 00:30:00 +0800

    Update tm1637.h

--- tm1637.h
+++ tm1637.h
@@ -7,88 +7,107 @@
 #define ADDR_FIXED 0x44
 #define START_ADDR 0xc0
 #define KEY_SCAN 0x42
-#define A 0x77
-#define b 0x7c
-#define C 0x39
-#define C_UP 0x61
-#define C_DOWN 0x5c
-#define d 0x5e
-#define E 0x79
-#define F 0x71
-#define G 0x3d
-#define MINUS 0x40
-#define O 0x3f
-#define O_UP 0x63
-#define O_DOWN 0x5c
-#define U 0x3e
-#define V 0x3e
-#define H 0x36
-#define J 0x0e
-#define L 0x38
-#define P 0x73
-#define NO_KEY B11111111
-#define K1S1 B11101111
-#define K1S2 B01101111
-#define K1S3 B10101111
-#define K1S4 B00101111
-#define K1S5 B11001111
-#define K1S6 B01001111
-#define K1S7 B10001111
-#define K1S8 B00001111
-#define K2S1 B11110111
-#define K2S2 B01110111
-#define K2S3 B10110111
-#define K2S4 B00110111
-#define K2S5 B11010111
-#define K2S6 B01010111
-#define K2S7 B10010111
-#define K2S8 B00010111
-static uint8_t DEFAULT_NUMS[] = {0x3f, 0x06, 0x5b, 0x4f,
-                                 0x66, 0x6d, 0x7d, 0x07,
-                                 0x7f, 0x6f};
+
 
 class TM1637
 {
 private:
-    boolean is_correct_ack;
-    uint8_t display_num_count_ = 4; //set the number of, more than 6 will be ignored
-    uint8_t brightness_ = 7;
-    uint8_t dio_pin_, clk_pin_;
+	boolean is_correct_ack = true;
+	uint8_t display_num_count_ = 4; //set the number of, more than 6 will be ignored
+	uint8_t brightness_ = 7;
+	uint8_t dio_pin_, clk_pin_;
 
-    void start();
+	void start();
 
-    void stop();
+	void stop();
 
-    void show(); //show the value
-    void write_byte(uint_least8_t data); //write byte value to chip
+	void show(); //show the value
+	void write_byte(uint_least8_t data); //write byte value to chip
 
 public:
+	enum PRESSED_KEY
+	{
+		NO_KEY =B11111111,
+		K1S1= B11101111,
+		K1S2= B01101111,
+		K1S3 =B10101111,
+		K1S4 =B00101111,
+		K1S5 =B11001111,
+		K1S6 =B01001111,
+		K1S7 =B10001111,
+		K1S8 =B00001111,
+		K2S1 =B11110111,
+		K2S2 =B01110111,
+		K2S3 =B10110111,
+		K2S4 =B00110111,
+		K2S5 =B11010111,
+		K2S6= B01010111,
+		K2S7= B10010111,
+		K2S8= B00010111
+	};
 
-    boolean getIsCorrectAck() const;
+	enum DISPLAY_NUM
+	{
+		NUM0=0x3f,
+		NUM1=0x06,
+		NUM2=0x5b,
+		NUM3=0x4f,
+		NUM4=0x66,
+		NUM5=0x6d,
+		NUM6=0x7d,
+		NUM7=0x07,
+		NUM8=0x7f,
+		NUM9=0x6f
+	};
 
-    TM1637(uint8_t dioPin, uint8_t clkPin);
+	enum DISPLAY_WORD
+	{
+		A= 0x77,
+		b= 0x7c,
+		C= 0x39,
+		C_UP= 0x61,
+		C_DOWN =0x5c,
+		d =0x5e,
+		E =0x79,
+		F =0x71,
+		G =0x3d,
+		MINUS =0x40,
+		O =0x3f,
+		O_UP= 0x63,
+		O_DOWN =0x5c,
+		U =0x3e,
+		V =0x3e,
+		H= 0x36,
+		J =0x0e,
+		L =0x38,
+		P =0x73
+	};
 
-    void set_brightness(uint8_t brightness);
+	boolean getIsCorrectAck() const;
+
+	TM1637(uint8_t dioPin, uint8_t clkPin);
+
+	void set_brightness(uint8_t brightness);
 
 
-    void set_display_num_count(uint8_t display_num_count);
+	void set_display_num_count(uint8_t display_num_count);
 
-    /*
-     display a number in position
-    */
-    void display(uint8_t index, uint8_t value, bool show_point);
+	/*
+	 display a number in position
+	*/
+	void display(uint8_t index, uint8_t value, bool show_point);
 
-    //put byte to the position
-    void display_byte(uint8_t index, uint8_t display_byte);
+	//put byte to the position
+	void display_byte(uint8_t index, uint8_t display_byte);
 
-    //display an array from 0 index
-    void display(const uint8_t *display_data, uint8_t length);
+	//display an array from 0 index
+	void display(const uint8_t* display_data, uint8_t length);
 
-    void clear_display();
+	void clear_display();
 
-    void test();
+	void test();
 
-    uint8_t scan_key();
+	PRESSED_KEY scan_key();
 };
 
 
